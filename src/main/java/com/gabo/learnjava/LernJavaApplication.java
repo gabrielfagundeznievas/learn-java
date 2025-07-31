291f4c69f227e41e923f5d590a1dc26f7cc67c5f
gabrielfagundeznievas <gabrielfagundeznievas@gmail.com>
2025-07-31 00:42:23 -0300

    chore: initialize Spring Boot project structure

---
+++ src/main/java/com/gabo/learnjava/LernJavaApplication.java
@@ -0,0 +1,13 @@
+package com.gabo.learnjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LernJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LernJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabo/learnjava/LernJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabo.learnjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LernJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
