e30d3fdf1d343d66c3a9a4c09031e0785a1c3283
gabrielfagundeznievas <gabrielfagundeznievas@gmail.com>
2025-07-31 01:20:06 -0300

    fix(core): correct project name across all files

--- src/main/java/com/gabo/learnjava/LearnJavaApplication.java
+++ src/main/java/com/gabo/learnjava/LearnJavaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LernJavaApplication {
+public class LearnJavaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LernJavaApplication.class, args);
+		SpringApplication.run(LearnJavaApplication.class, args);
 	}
 
 }
